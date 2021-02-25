c69e2811547388ee1c024d9f301adc513c4c17ae
RizwandaaK <rizwanda_keysha_29rpl@student.smktelkom-mlg.sch.id>
2021-02-25 21:09:19 +0700

    KalkulatorNonVoid

---
+++ src/kalkulatornonvoid/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulatornonvoid;
+
+/**
+ *
+ * @author rizwa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        KalkulatorNonVoid ijiran = new KalkulatorNonVoid();
+        ijiran.KalkulatorNonVoid(500, 8);
+        ijiran.penjumlahan();
+        ijiran.pengurangan();
+        ijiran.perkalian();
+        ijiran.pembagian();
+    }
+    
+}
